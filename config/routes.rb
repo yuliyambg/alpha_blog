816aa23c3fcf0bb44129474adbbdaf44b8f92343
yuliyambg <yuliyambg@gmail.com>
2020-05-23 11:53:33 -0400

    Alpha blog signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+
 end
