7102a1b4dbfa195315ebb2c04450a348eb20e086
yuliyambg <yuliyambg@gmail.com>
2020-06-14 12:36:23 -0400

    Alpha Blog final version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_231909) do
+ActiveRecord::Schema.define(version: 2020_06_13_191121) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
